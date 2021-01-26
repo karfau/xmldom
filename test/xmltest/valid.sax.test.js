06e1bc7c7550b471ade1fac95c5267403a9d869b
Christian Bewernitz <coder@karfau.de>
2021-01-26 09:12:14 +0100

    test: Improve readability of snapshots
    and don't parse `/out/` folders

--- test/xmltest/valid.sax.test.js
+++ test/xmltest/valid.sax.test.js
@@ -4,9 +4,20 @@ const xmltest = require('xmltest')
 const { SaxHandler } = require('xmltest/sax')
 const { XMLReader } = require('../../lib/sax')
 
+const reduceCallEntry = (callEntry) => {
+	const simple = callEntry.filter((it) => !!it && !(typeof it === 'object'))
+	return simple.length === 1
+		? simple[0]
+		: xmltest.replaceNonTextChars(simple.join(',')).replace(
+			/[\r\n]/g,
+			xmltest.replaceWithWrappedCodePointAt
+		)
+}
+const toSnapshotString = (callEntries) => callEntries.map(reduceCallEntry).join("\n");
+
 describe('xmltest/valid', () => {
 	describe.only('sax', () => {
-		test.each(Object.keys(xmltest.getEntries(xmltest.FILTERS.xml)))(
+		test.each(Object.keys(xmltest.getEntries(xmltest.FILTERS.xml, it => !/\/out\//.test(it))))(
 			'should parse %s',
 			async (pathInZip) => {
 				const input = await xmltest.getContent(pathInZip)
@@ -17,7 +28,7 @@ describe('xmltest/valid', () => {
 
 				it.parse(input, {}, {})
 
-				expect(handler.getCallsInOrder()).toMatchSnapshot()
+				expect(toSnapshotString(handler.getCallsInOrder())).toMatchSnapshot()
 			}
 		)
 	})
