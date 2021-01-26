a5769f0988ba46cda7a5bad0586f61af6f63855f
Christian Bewernitz <coder@karfau.de>
2021-01-26 09:19:15 +0100

    test: Only consider valid xml for now
    and apply prettier

--- test/xmltest/valid.sax.test.js
+++ test/xmltest/valid.sax.test.js
@@ -8,28 +8,33 @@ const reduceCallEntry = (callEntry) => {
 	const simple = callEntry.filter((it) => !!it && !(typeof it === 'object'))
 	return simple.length === 1
 		? simple[0]
-		: xmltest.replaceNonTextChars(simple.join(',')).replace(
-			/[\r\n]/g,
-			xmltest.replaceWithWrappedCodePointAt
-		)
+		: xmltest
+				.replaceNonTextChars(simple.join(','))
+				.replace(/[\r\n]/g, xmltest.replaceWithWrappedCodePointAt)
 }
-const toSnapshotString = (callEntries) => callEntries.map(reduceCallEntry).join("\n");
+const toSnapshotString = (callEntries) =>
+	callEntries.map(reduceCallEntry).join('\n')
 
 describe('xmltest/valid', () => {
 	describe.only('sax', () => {
-		test.each(Object.keys(xmltest.getEntries(xmltest.FILTERS.xml, it => !/\/out\//.test(it))))(
-			'should parse %s',
-			async (pathInZip) => {
-				const input = await xmltest.getContent(pathInZip)
-				const handler = SaxHandler()
-				const it = new XMLReader()
-				it.domBuilder = handler
-				it.errorHandler = handler
+		test.each(
+			Object.keys(
+				xmltest.getEntries(
+					"xmltest/valid/",
+					xmltest.FILTERS.xml,
+					(it) => !/\/out\//.test(it)
+				)
+			)
+		)('should parse %s', async (pathInZip) => {
+			const input = await xmltest.getContent(pathInZip)
+			const handler = SaxHandler()
+			const it = new XMLReader()
+			it.domBuilder = handler
+			it.errorHandler = handler
 
-				it.parse(input, {}, {})
+			it.parse(input, {}, {})
 
-				expect(toSnapshotString(handler.getCallsInOrder())).toMatchSnapshot()
-			}
-		)
+			expect(toSnapshotString(handler.getCallsInOrder())).toMatchSnapshot()
+		})
 	})
 })
